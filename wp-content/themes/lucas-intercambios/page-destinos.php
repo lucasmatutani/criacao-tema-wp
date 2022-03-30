c7ae857408dc00a169291e318a9f702c090bf0c8
lucasmatutani <lucasmatutani@gmail.com>
2022-03-30 14:56:58 -0300

    Criação das páginas

---
+++ wp-content/themes/lucas-intercambios/page-destinos.php
@@ -0,0 +1,33 @@
+<?php
+$estiloPagina = 'destinos.css';
+require_once 'header.php';
+?>
+
+<form action="#" class="container-header formulario-pesquisa-paises" >
+    <h2>Conheça nossos destinos</h2>
+    <select name="paises" id="paises">
+        <option value="">--Selecione--</option>
+        <?php
+            get_terms(array('taxonomy'=>'paises'));
+        ?>
+    </select>
+    <input type="submit" value="pesquisar">
+</form>
+
+<?php
+$args = array('post_type' => 'destinos' );
+$query = new WP_Query($args);
+if ($query->have_posts()):
+    echo '<main class="page-destinos">';
+    echo '<ul class="lista-destinos container-alura">';
+    while($query->have_posts()): $query->the_post();
+        echo '<li class="col-md-3 destinos" >';
+        the_post_thumbnail();
+        the_title('<p class="titulo-destino">','</p>');
+        the_content();
+        echo '</li>';
+    endwhile;
+    echo '</ul>';
+    echo '</main>';
+endif;
+require_once 'footer.php';
